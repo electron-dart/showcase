a8a75e70b816e3bfb7bc5a72b6700ffdb47a83bb
Ravi Teja Gudapati <tejainece@gmail.com>
2016-09-15 13:59:07 +0200

    BrowserWindow and NativeImage skeletons

---
+++ README.md
@@ -0,0 +1,9 @@
+# showcase
+
+Show case of various electron.dart features.
+
+It is also intended for manual testing purposes.
+
+## Warning
+
+This is under development so if you have suggestion on the existing API or you find a bug don't hesitate to fill an issue :)
---
+++ web/main_process/common/native_image/main.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2016, electron.dart. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import "package:nodejs/nodejs.dart" as nodejs;
+import "package:electron/main.dart" as tron;
+
+main() {
+  tron.app.onWindowAllClosed.listen((_) {
+    if (nodejs.process.platform != "darwin") {
+      tron.app.quit();
+    }
+  });
+
+  tron.app.onReady.listen((_) {
+    final win = new tron.BrowserWindow();
+    win.loadURL("file://${nodejs.dirname}/client/index.html");
+    final imgEmpty = tron.createEmpty();
+  });
+}
